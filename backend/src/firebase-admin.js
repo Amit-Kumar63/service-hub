efeedcf4bc7fd617e8b8179b7c2e8c37dba5cdf9
Amit-Kumar63 <amitkumarpro76@email.com>
2025-03-04 23:23:25 +0530

    Fix Firebase admin initialization to parse credentials from environment variable

--- backend/src/firebase-admin.js
+++ backend/src/firebase-admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = process.env.FIREBASE_CREDENTIALS
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
