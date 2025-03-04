8753f7c0dd46187a884e0cfc20ad58f9861ae421
Amit-Kumar63 <amitkumarpro76@email.com>
2025-03-04 23:16:49 +0530

    Refactor Firebase admin initialization to use environment variable for credentials

--- backend/src/firebase-admin.js
+++ backend/src/firebase-admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../service-hub-e4931-firebase-adminsdk-fbsvc-0f5cc632b6.json");
+const serviceAccount = process.env.FIREBASE_CREDENTIALS
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
